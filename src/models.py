e5706e7728ade99b04b17762cda23c68b6da7169
ihorSmachylo <igoor.smachyloo@gmail.com>
2023-03-01 15:18:34 +0000

    star ward modeling

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, Boolean, DateTime, Float, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -9,57 +9,51 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-
+# Web user data
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = "user"
     id = Column(Integer, primary_key=True)
-    username = Column(String(255))
-    firstname = Column(String(255))
-    lastname = Column(String(255))
-    email = Column(String(100))
-    posts = relationship("Post",)
-    comments = relationship("Comment")
-
-
-class Post(Base):
-    __tablename__ = 'post'
+    created_date = Column(DateTime)
+    name = Column(String(50), nullable=False)
+    lastname = Column(String(100), nullable=False)
+    email = Column(String(70), nullable=False)
+    password = Column(String(15), nullable=False)
+    is_active = Column(Boolean)
+    
+class Planet(Base):
+    __tablename__ = "planet"
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship('User')
-    medias = relationship("Media")
-    comments = relationship("Comment")
+    name = Column(String(100), nullable=False)
+    temperature = Column(Float)
+    image_url = Column(String(250))
 
-
-class Media(Base):
-    __tablename__ = 'media'
+class Vehicle(Base):
+    __tablename__ = "vehicle"
     id = Column(Integer, primary_key=True)
-    type = Column(String(200))
-    url = Column(String(255))
-    post_id = Column(Integer, ForeignKey('post.id'))
-    post = relationship(Post)
+    name = Column(String(100), nullable=False)
+    price = Column(Float)
+    image_url = Column(String(250))
 
-
-class Comment(Base):
-    __tablename__ = 'comment'
+class Character(Base):
+    __tablename__ = "character"
     id = Column(Integer, primary_key=True)
-    comment_text = Column(String(1000))
-    author_id = Column(Integer, ForeignKey('author.id'))
-    post_id = Column(Integer, ForeignKey('post.id'))
-    user = relationship(User)
-    post = relationship(Post)
+    name = Column(String(100), nullable=False)
+    description = (String(200))
+    birthday = Column(DateTime, nullable=False)
+    image_url = Column(String(250))
+    planet_id = Column(Integer, ForeignKey("planet.id"))
+    vehicle_id = Column(Integer, ForeignKey("vehicle.id"))
+    planet = relationship(Planet)
+    vehicle = relationship(Vehicle)
 
-
-class Follower(Base):
-    __tablename__ = 'follower'
+# List of all user favourites
+class Favourite(Base):
+    __tablename__ = "favourite"
     id = Column(Integer, primary_key=True)
-    user_from_id = Column(Integer, ForeignKey('user.id'))
-    user_to_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
-
+    created_date = Column(DateTime)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    character_id = Column(Integer, ForeignKey("character.id"))
+    planet_id = Column(Integer, ForeignKey("planet.id"))
+    
 ## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+render_er(Base, "diagram.png")
